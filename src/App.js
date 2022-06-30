ff700bd4026b7030aaf7e0540c098fa49fa9bda2
Restyled.io <commits@restyled.io>
2022-06-30 15:05:36 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import {productInputs, userInputs} from "./formSource";
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
 import List from "./pages/list/List";
-import Single from "./pages/single/Single";
+import Login from "./pages/login/Login";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { productInputs, userInputs } from "./formSource";
+import Single from "./pages/single/Single";
 
 function App() {
 
@@ -13,22 +14,33 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
+            <Route index element={
+<Home />} />
+            <Route path="login" element={<Login />
+}
+/>
             <Route path="users">
-              <Route index element={<List />} />
-              <Route path=":userId" element={<Single />} />
+              <Route index element={<List / >
+}
+/>
+              <Route path=":userId" element={<Single / >
+}
+/>
               <Route
                 path="new"
-                element={<New inputs={userInputs} title="Add New User" />}
-              />
-            </Route>
-            <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
+                element={<New inputs={userInputs} title="Add New User" / >
+}
+/>
+            </Route > <Route path = "products"><Route index element = {
+  <List />
+} />
+              <Route path=":productId" element={<Single />
+}
+/>
               <Route
                 path="new"
-                element={<New inputs={productInputs} title="Add New Product" />}
+                element={<New inputs={productInputs} title="Add New Product" / >
+}
               />
             </Route>
           </Route>
@@ -36,6 +48,6 @@ function App() {
       </BrowserRouter>
     </div>
   );
-}
+              }
 
-export default App;
+              export default App;
--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,18 +1,13 @@
+import "./list.scss";
+
 import Datatable from "../../components/datatable/Datatable";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
-import "./list.scss";
 
 const List = () => {
-  return (
-    <div className="list">
-      <Sidebar />
-      <div className="listContainer">
-        <Navbar />
-        <Datatable />
-      </div>
-    </div>
-  );
+  return (<div className = "list"><Sidebar /><div className = "listContainer">
+          <Navbar /><Datatable /></div>
+    </div>);
 };
 
 export default List;
